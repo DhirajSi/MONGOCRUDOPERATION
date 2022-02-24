485bf0da792586240003260747151144e0f99915
Dhiraj Singh <dhisingh3@publicisgroupe.net>
2022-02-24 14:28:31 +0530

    third commit

--- utility/utility.js
+++ utility/utility.js
@@ -1,90 +1,16 @@
 const fs = require("fs");
 
-// //loger function
-// function printlog(data) {
-//   //  var currentdate = new Date();
+//loger function
+function printlog(data) {
+  //  var currentdate = new Date();
 
-//   fs.appendFile("log.txt", data, function (err) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Succesfully print log into file");
-//      fs.appendFile("logbook.txt","123",function(err){
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("log book maintain");
-//       }
-
-//      })
-//     }
-//   });
-// }
-
-// printlog("log1");
-
-function asynPrintLog(data) {
-  return new Promise((resolve, reject) => {
-    fs.appendFile("log.txt", data, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve("Succesfully print log into file");
-      }
-    });
+  fs.appendFile("log.txt", data, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Succesfully print log into file");
   });
 }
-
-function asynPrintLogBook(data) {
-  return new Promise((resolve, reject) => {
-    fs.appendFile("logbook.txt", "12345", function (err) {
-      if (err) {
-        resolve("log book maintain");
-      } else {
-      
-        reject("errorrrrrrrrrrrrrrr");
-      }
-    });
-  });
-}
-
-// asynPrintLog("hello")
-//   .then((msg) => {
-//     console.log(msg);
-//     asynPrintLogBook("hiiiiii")
-//       .then((msg2) => {
-//         console.log(msg2);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-async function createLog(){
-  try {
-    const msg=await asynPrintLog("newabcd");
-    console.log(msg);
-
-    const msg2=await asynPrintLogBook("newlogbookmsg");
-    console.log(msg2)
-    
-  } catch (error) {
-    console.log("##########"+error)
-  }
-
-}
-
-createLog();
-
-
-
-
-
-
-
 
 //capture current date and time method
 var currentdate = new Date();
@@ -102,7 +28,7 @@ var dateTime =
   ":" +
   currentdate.getSeconds();
 
-// module.exports = {
-//   dateTime,
-//   printlog,
-// };
+module.exports = {
+  dateTime,
+  printlog,
+};
